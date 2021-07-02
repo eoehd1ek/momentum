4d45be1c6acc01e7d34d67b0756d17d433654eb7
eoehd1ek <eoehd1ek@naver.com>
2021-07-03 03:13:37 +0900

    Feat: "추가 랜덤 배경 화면 표시 기능"

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["0.jpg", "1.jpg", "2.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+bgImage.width = window.innerWidth
+bgImage.height = window.innerHeight;
+document.body.appendChild(bgImage);
