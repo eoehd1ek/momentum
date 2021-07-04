b28456b3846c9ed468bc20a8a31bada56a9d978e
eoehd1ek <eoehd1ek@naver.com>
2021-07-04 19:31:11 +0900

    Design: "변경 명언 내용"

--- js/quotes.js
+++ js/quotes.js
@@ -1,43 +1,27 @@
 const quotes = [
     {
-        quote: "string1",
-        author: "name1"
+        quote: "하루에 3시간을 걸으면 7년 후에 지구를 한바퀴 돌 수 있다.",
+        author: "사무엘 존슨"
     },
     {
-        quote: "string2",
-        author: "name2"
+        quote: "오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다.",
+        author: "앙드레 말로"
     },
     {
-        quote: "string3",
-        author: "name3"
+        quote: "사막이 아름다운 것은 어딘가에 샘이 숨겨져 있기 때문이다.",
+        author: "생텍쥐페리"
     },
     {
-        quote: "string4",
-        author: "name4"
+        quote: "내일은 내일의 태양이 뜬다.",
+        author: "in 바람과 함께 사라지다"
     },
     {
-        quote: "string5",
-        author: "name5"
+        quote: "절대 어제를 후회하지 마라. 인생은 오늘의  내 안에 있고 내일은 스스로 만드는것이다.",
+        author: "L. 론 허바드"
     },
     {
-        quote: "string6",
-        author: "name6"
-    },
-    {
-        quote: "string7",
-        author: "name7"
-    },
-    {
-        quote: "string8",
-        author: "name8"
-    },
-    {
-        quote: "string9",
-        author: "name9"
-    },
-    {
-        quote: "string10",
-        author: "name10"
+        quote: "해야 할 것을 하라. 모든 것은 타인의 행복을 위해서, 동시에 특히 나의 행복을 위해서이다.",
+        author: "톨스토이"
     }
 ];
 
@@ -46,4 +30,4 @@ const author = document.querySelector("#quote span:last-child")
 
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+author.innerText = " · " + todaysQuote.author;
